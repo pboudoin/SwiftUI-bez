ae97f846cddf7a532b282a7fd546bb70d59cfe09
Pierre Boudoin <pierre.boudoin@gmail.com>
2022-02-08 13:55:14 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             targets: ["bez"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/kieranb662/CGExtender.git", from: "1.0.1")
+        .package(url: "https://github.com/pboudoin/CGExtender.git", from: "1.0.1")
     ],
     targets: [
         .target(
